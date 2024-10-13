f7139b4a2d075615ea1302fff6c7c5f5c9b1f135
arttturslv <arttturslv@gmail.com>
2024-10-13 20:43:48 -0300

    feat: added static information component

---
+++ src/components/HomeInformation.jsx
@@ -0,0 +1,13 @@
+export default function HomeTitle() {
+    return (
+        <div className="py-16">
+          <div className="text-seaSalt">
+            <h1 className="text-6xl py-4 text-center">Minimiza URL</h1>
+            <span>
+              <p className="text-center">Minimiza URL é uma ferramenta gratuita para gerar links encurtados, permitindo que você compartilhe URLs longas de forma mais prática e conveniente. </p>
+              <p className="text-center">Ideal para redes sociais, e-mails ou qualquer plataforma onde o espaço é limitado.</p>
+            </span>
+          </div>
+      </div>
+    )
+}
