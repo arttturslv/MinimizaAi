a63bc2c252a7add012a570ee8bb0f88348d917de
arttturslv <arttturslv@gmail.com>
2024-10-13 20:44:51 -0300

    feat: added components to home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Input from "./components/Input/Input";
+import HomeInformation from "./components/HomeInformation";
+
 function App() {
 
   return (
-    <p >Hello World</p>
+    <div className="bg-jet w-full flex flex-col h-screen">
+      <HomeInformation/>
+      <Input/>
+      <Footer/>
+    </div>
   )
 }
 
