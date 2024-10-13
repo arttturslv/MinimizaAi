a793cf07f686e562398da205f39dd0ffc7d92551
arttturslv <arttturslv@gmail.com>
2024-10-13 20:39:38 -0300

    assets: added colors & icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ export default {
     colors: {
       jet:"#2C2D31",
       eerieBlack:"#161819",
+      eerieBlackLight:"#1F2021",
+      eerieBlackDark:"#1A1D1E",
       seaSalt:"#F7F9F9",
       celticBlue:"#4472CA"
     }
