daa182aaa24e53b1b7f4bf58f9fd40da24b035a7
arttturslv <arttturslv@gmail.com>
2024-10-13 19:00:39 -0300

    build: added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      jet:"#2C2D31",
+      eerieBlack:"#161819",
+      seaSalt:"#F7F9F9",
+      celticBlue:"#4472CA"
+    }
+  },
+  plugins: [],
+}
