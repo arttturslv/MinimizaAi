a0eebf82726ceac6af741c7d094b1a7280f0533e
arttturslv <arttturslv@gmail.com>
2024-10-14 17:42:04 -0300

    feat: added API conection & localStorage persistence

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,20 +1,54 @@
 import History from "./History"
 import { useState } from "react"
+import {encurtar} from "../../API/useApi";
 
 export default function Input({togglePoliticaVisibility}) {
     const [inputValue, setInputValue] = useState('');
     const [isURLValid, setIsURLValid] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const [hasError, setHasError] = useState(false);
 
-    function handleSend(e) {
+
+    async function handleSend(e) {
         e.preventDefault();
         console.log("enviar")
+
+        setIsLoading(true); 
+        const {data, error} = await encurtar(inputValue);
+        setIsLoading(false);
+
+        if(error) {
+            setHasError(error);
+            return;
+        }
+
+        storeLocally(data);
+
     }
+
+    function storeLocally(response) {
+
+        const objectURL = {
+            shortLink : response.encurtado,
+            original : response.original,
+            visitas : response.visitas,
+            data : response.data
+        }
+
+        let allLinks = localStorage.getItem("allLinks");
+        allLinks = allLinks != null ? JSON.parse(allLinks) : [];
+        allLinks.push(objectURL);
+
+        localStorage.setItem('allLinks', JSON.stringify(allLinks))
+    }
+
 
 
     function handleChange(e) {
         e.preventDefault();
         setInputValue(e.currentTarget.value);
-        setIsURLValid(hasValidURL(inputValue));
+        setIsURLValid(hasValidURL(e.currentTarget.value));
     }
 
     function hasValidURL(input) {
@@ -24,7 +58,6 @@ export default function Input({togglePoliticaVisibility}) {
             console.log(url)
             return true;
         } catch (error) {
-
             return false;
         }   
     }
@@ -33,13 +66,20 @@ export default function Input({togglePoliticaVisibility}) {
         <div className="w-full flex justify-center flex-1">
             <div className="bg-eerieBlackLight rounded-3xl h-min">
                 <div className="bg-eerieBlack px-8 py-5 rounded-3xl">
-                    <form  action="">
+                    <form onSubmit={(e)=>handleSend(e)} action="">
                         <div className=" flex gap-3">
-                            <input onChange={(e)=> handleChange(e)} onSubmit={()=>handleSend(e)} value={inputValue} className="max-w-[640px] w-full bg-jet px-4 py-2 rounded-xl text-seaSalt" type="text" name="" id="" placeholder="Insira o link aqui" />
+                            <input onChange={(e)=> handleChange(e)} value={inputValue} className="max-w-[640px] w-full bg-jet px-4 py-2 rounded-xl text-seaSalt" type="text" name="" id="" placeholder="Insira o link aqui" />
                             
                             <button  type="submit" >
                                 <div className="p-2 bg-eerieBlack rounded-xl hover:bg-eerieBlack/60 cursor-pointer">
-                                    <img width={32} height={32} src="/src/assets/arrow-right.png" alt="icone de enviar" />
+                                   
+                                   {
+                                     !isLoading?
+                                     <img width={32} height={32} src="/src/assets/arrow-right.png" alt="icone de enviar" />
+                                     :
+                                     <img width={32} height={32} src="/src/assets/loading.png" alt="icone de carregamento" />
+
+                                   }
                                 </div>
                             </button>
                         </div>
