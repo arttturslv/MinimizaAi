5a30a52d762e3415182a3bde10bbe3e174286652
arttturslv <arttturslv@gmail.com>
2024-10-15 19:36:36 -0300

    refac: refactored button

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,5 @@
 import History from "./History"
+import Button from "../Button";
 import { useState, useEffect } from "react"
 import {encurtar} from "../../API/useApi";
 
@@ -87,21 +88,13 @@ export default function Input({togglePoliticaVisibility}) {
                     <form onSubmit={(e)=>handleSend(e)} action="">
                         <div className=" flex gap-3">
                             <input onChange={(e)=> handleChange(e)} value={inputValue} className="shadow-s-input w-[640px] max-sm:w-full bg-jet px-4 py-2 rounded-xl text-seaSalt" type="text" name="" id="" placeholder="Insira o link aqui" />
-                            
-                            <button disabled={isLoading} type="submit" >
-                                <div className="p-2 bg-eerieBlack rounded-xl shadow-s-input-box transition-all hover:bg-eerieBlack/60 cursor-pointer">
-                                   
-                                   {
-                                     !isLoading?
-                                     <img width={32} height={32} src="https://i.imgur.com/50Y8SLg.png" alt="icone de enviar" />
-                                     :
-                                     <img width={32} height={32} className=" animate-spin" src="https://i.imgur.com/Jv0GKTg.png" alt="icone de carregamento" />
-
-                                   }
-                                </div>
-                            </button>
+                            <Button hasShadow={true} isLoading={isLoading} disabled={isLoading} >                    
+                                <img width={32} height={32} src="https://i.imgur.com/50Y8SLg.png" alt="icone de enviar" />
+                            </Button>
                         </div>
-                        { !isURLValid && <p className="text-xs text-[red]/40 -mb-2">A link digitado não é válido! Verifique a presença do protocolo: "https://".</p>}
+                        { 
+                            !isURLValid && <p className="text-xs text-[red]/40 -mb-2">A link digitado não é válido! Verifique a presença do protocolo: "https://".</p>
+                        }
                     </form>
                 </div>
                 <History togglePoliticaVisibility={togglePoliticaVisibility} urlList={urlList} setUrlList={setUrlList}/>
