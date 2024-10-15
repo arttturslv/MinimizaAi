3325af45013c1354d27142b68a72416c482f6cbc
arttturslv <arttturslv@gmail.com>
2024-10-15 19:36:16 -0300

    feat: added loading state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-export default function Button ({children, hasShadow=false, ...props})  {
+export default function Button ({children, hasShadow=false, isLoading=false, ...props})  {
     return (
         <button {...props} type="submit" >
             <div className={`p-2 bg-eerieBlack rounded-xl ${hasShadow?"shadow-s-input-box":null} transition-all hover:bg-eerieBlack/40 cursor-pointer`}>
-                {children}
+                { !isLoading ?
+                    children
+                    :
+                    <img width={32} height={32} className=" animate-spin" src="https://i.imgur.com/Jv0GKTg.png" alt="icone de carregamento" />
+                }
             </div>
         </button>
     )
