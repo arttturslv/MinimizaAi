f74e91bc54ceabaf21473899dd3f0db8444bdffe
arttturslv <arttturslv@gmail.com>
2024-10-15 19:22:27 -0300

    refac: refactored function & custom button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,21 @@
-export default function Footer({togglePoliticaVisibility}) {
+import Button from '../components/Button';
+import {openURL} from '../API/Utils.js'
 
-    function handleGithub() {
-        window.open("https://www.github.com/arttturslv", "_blank")
-    }
+export default function Footer({togglePoliticaVisibility}) {
 
     return (
         <div className="w-full flex justify-center mt-16 ">
             <div className="bg-eerieBlackLight flex gap-4 px-4 py-2 my-2 rounded-xl shadow-s-input-box">
             <button type="button" onClick={togglePoliticaVisibility} className="bg-eerieBlack px-4 py-3 rounded-xl cursor-pointer group hover:bg-eerieBlack/60 ">
-                <p className="text-seaSalt group-hover:text-celticBlue transition-colors">Acessar politicas de privacidade</p>
+                <p className="text-seaSalt group-hover:text-celticBlue transition-colors">Acessar políticas de privacidade</p>
             </button>
             <div className="flex gap-3">
                 <div className="bg-eerieBlack px-4 py-3 rounded-xl max-sm:hidden">
                     <p className="text-seaSalt">Esse aplicativo foi desenvolvido pelo Artttur</p>
                 </div>
-                <div className="p-2 flex items-center bg-eerieBlack rounded-xl hover:bg-eerieBlack/60 cursor-pointer">
-                    <img width={32} height={32} src="https://i.imgur.com/Ka5k2Wa.png" onClick={handleGithub} alt="icone do github" />
-                </div>
+                <Button onClick={() => openURL("https://www.github.com/arttturslv")} >                    
+                    <img width={32} height={32} src="https://i.imgur.com/Ka5k2Wa.png" alt="icone do github" />
+                </Button>
             </div>
             </div>
         </div>
