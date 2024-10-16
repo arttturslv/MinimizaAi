c9f61ac0054e62370170bed428f36704e1228599
arttturslv <arttturslv@gmail.com>
2024-10-15 21:47:38 -0300

    style: added favicon

--- src/components/HomeInformation.jsx
+++ src/components/HomeInformation.jsx
@@ -2,10 +2,10 @@ export default function HomeTitle() {
     return (
         <div className="py-16">
           <div className="text-seaSalt">
-            <h1 className="text-6xl py-4 text-center">Minimiza URL</h1>
+            <h1 className="text-6xl py-4 text-center">Minimiza aí</h1>
             <span className="flex justify-center">
               <div className="w-[80%]">
-                <p className="text-center">Minimiza URL é uma ferramenta gratuita para gerar links encurtados. </p>
+                <p className="text-center"><strong>Minimiza aí</strong> é uma ferramenta gratuita para gerar links encurtados. </p>
                 <p className="text-center">Ideal para redes sociais, e-mails ou qualquer plataforma onde o espaço é limitado.</p>
               </div>
             </span>
